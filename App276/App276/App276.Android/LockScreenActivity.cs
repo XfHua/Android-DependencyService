f73e90bf9df2956b0d9ab3e8641a3aeb93027322
XfHua <jasonHuaxf@163.com>
2020-06-16 10:27:00 +0800

    Add files via upload

---
+++ App276/App276/App276.Android/LockScreenActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace App276.Droid
+{
+    [Activity(Label = "LockScreenActivity", Theme = "@style/MainTheme")]
+    public class LockScreenActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+
+            TabLayoutResource = Resource.Layout.Tabbar;
+            ToolbarResource = Resource.Layout.Toolbar;
+
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+
+            SetContentView(Resource.Layout.layout1);
+        }
+    }
+}
---
+++ App276/App276/App276.Android/LockScreenAndroid.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using App276.Droid;
+using Xamarin.Forms;
+
+[assembly: Dependency(typeof(LockScreenAndroid))]
+namespace App276.Droid
+{
+    public class LockScreenAndroid : ILockScreenService
+    {
+        public void lockScreen()
+        {
+            var second = new Intent(Android.App.Application.Context, typeof(LockScreenActivity));
+            second.AddFlags(ActivityFlags.NewTask);
+            Android.App.Application.Context.StartActivity(second);
+        }
+    }
+}
